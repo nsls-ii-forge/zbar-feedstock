5480187ee00b6df45e3e68de425d38c2f3e0c43c
Maksim Rakitin <mrakitin@bnl.gov>
2020-06-10 17:30:27 -0400

    Initial feedstock commit with conda-smithy 3.7.3.

---
+++ recipe/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./configure --prefix=$PREFIX  --disable-video --without-gtk --without-python \
+            --without-qt --without-imagemagick --without-xv --without-x \
+            --without-xshm
+make
+make check
+make install
